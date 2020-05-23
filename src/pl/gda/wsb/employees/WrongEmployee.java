aa67147e059ca3fd4987c400c78dc0841fb81fa1
sosno <msosnowski@wsb.gda.pl>
2020-05-23 17:26:55 +0200

    Zadanie 11 wyjątek WrongEmployee

---
+++ src/pl/gda/wsb/employees/WrongEmployee.java
@@ -0,0 +1,7 @@
+package pl.gda.wsb.employees;
+
+public class WrongEmployee extends Exception{
+    public WrongEmployee(){
+        System.out.println("Nie znaleziono pracownika w bazie!\n\nProszę ponownie podać imię i nazwisko pracownika (exit = koniec):");
+    }
+}
